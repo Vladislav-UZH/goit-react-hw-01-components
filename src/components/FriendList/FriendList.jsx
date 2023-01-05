5f3ba8fe76ef4b3d4aa722f0dad671d817de3103
Vladislav-UZH <perebziak.vladislav@gmail.com>
2023-01-06 00:17:17 +0200

    finished Friends card

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,11 +2,9 @@ import { FriendListItem } from 'components/FriendListItem/FriendListItem';
 import PropTypes from 'prop-types';
 import { List } from './FriendsList.styled';
 export const FriendList = ({ props }) => {
-  //   console.log(props);
   return (
     <List>
       {props.map(friend => {
-        // console.log(friend);
         return <FriendListItem key={friend.id} props={friend} />;
       })}
     </List>
